0bebac8d1b86fd9ec805f214e13b1cca6985046b
Josh Gillman <jegillman@oakland.edu>
2016-02-04 16:49:08 -0500

    Added empty main() and recursive song java code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        
+    }
+
+    public static int Ninety_Nine_Bottles_of_Beer(int count) {
+        if (count > 2) {
+            System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer,\n" +
+                    "take one down, pass it around, " + (count - 1) + " bottles of beer on the wall");
+            return Ninety_Nine_Bottles_of_Beer(count - 1);
+        }
+        System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer,\n" +
+                "take one down, pass it around, " + (count - 1) + " bottle of beer on the wall.\n" +
+                (count - 1) + " bottle of beer on the wall, " + (count - 1) + " bottle of beer,\n" +
+                "take one down, pass it around, no more bottles of beer on the wall.");
+        return 0;
     }
 }
